022f6094473032824f12e0f993bdd7d2dc2edf61
Anna Dominiak <dominiakq@gmail.com>
2025-06-12 11:19:37 +0200

    Start nie jako klasa + main.

--- start.py
+++ start.py
@@ -1,46 +1,44 @@
-class Start:
-    def run(self):
-        while True:
-            self.print_available_actions()
-            user_choice = input("Wybierz, co chcesz zrobić: ")
-            result = self.action_handler(user_choice)
-            if not result:
-                break
-            return False
+def print_available_actions():
+    print("\nDostępne akcje:")
+    print("1. Login")
+    print("2. Rejestracja")
+    print("3. Wyjście")
 
-    def print_available_actions(self):
-        print("\nDostępne akcje:")
-        print("1. Login")
-        print("2. Rejestracja")
-        print("3. Wyjście")
+def action_handler(user_choice, user_list):
+    user_choice = user_choice.lower()
 
-    def action_handler(self, user_choice):
-        if user_choice.lower == "Login":
-            print("\nPodaj dane logowania:")
-            login = input("Login: ")
-            password = input("Hasło: ")
-            user = user_list.login(login, password)
-            if user:
-                user.run()
-                return True
-            else:
-                print("Nieprawidłowy login lub hasło.")
-                return True
-
-        elif user_choice.lower == "Register":
-            print("\nZarejestruj się:")
-            login = input("Podaj login: ")
-            password = input("Utwórz hasło: ")
-            if user_list.register(login, password):
-                print("Rejestracja powiodła się. Możesz się teraz zalogować.")
-            else:
-                print("Użytkownik o takim loginie już istnieje.")
-            return True
-
-        elif user_choice.lower == "Exit":
-            print("Zamykanie programu")
-            return False
-
+    if user_choice in ("1", "login"):
+        print("\nPodaj dane logowania:")
+        login = input("Login: ")
+        password = input("Hasło: ")
+        user = user_list.login(login, password)
+        if user:
+            user.run()
         else:
-            print("Nieznana akcja")
-            return True
+            print("Nieprawidłowy login lub hasło.")
+        return True
+
+    elif user_choice in ("2", "rejestracja", "register"):
+        print("\nZarejestruj się:")
+        login = input("Podaj login: ")
+        password = input("Utwórz hasło: ")
+        if user_list.register(login, password):
+            print("Rejestracja powiodła się. Możesz się teraz zalogować.")
+        else:
+            print("Użytkownik o takim loginie już istnieje.")
+        return True
+
+    elif user_choice in ("3", "exit", "wyjście"):
+        print("Zamykanie programu.")
+        return False
+
+    else:
+        print("Nieznana akcja.")
+        return True
+
+def main_menu(user_list):
+    while True:
+        print_available_actions()
+        user_choice = input("Wybierz, co chcesz zrobić: ")
+        if not action_handler(user_choice, user_list):
+            break
