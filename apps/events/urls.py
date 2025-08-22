367272ea15b04fb4ca7ac73e5cd25173fdb11ebf
Mateusz Dobosz <klenchere@gmail.com>
2025-08-22 19:39:24 +0100

    Dodano przycisk "Wyświetl szczegóły" dla wydarzeń i poprawiono stylowanie przycisków w szablonach.

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import event_list_view
+from .views import event_list_view, event_detail_view
 
 app_name = "events"
 
 urlpatterns = [
     path('', event_list_view, name='events_list'),
+    path('event_detail/<int:id>/', event_detail_view, name='event_detail'),
 ]
