2b3d48287bf0830d0ea15ab1af8dd9bd156b6372
sebescen999 <marcin.laskowski.vc@gmail.com>
2025-10-11 18:16:16 +0200

    Zmiany: -> dodany logger.py -> zmiany we views.py dla poszczególnych aplikacji (loggery) -> małe korekty - usunięcie niepotrzebnych komentarzy

--- apps/booking/forms.py
+++ apps/booking/forms.py
@@ -1,5 +1,4 @@
 from django import forms
 
 class BookingForm(forms.Form):
-    # Usuń pola full_name i email - będą pobierane z zalogowanego użytkownika
     quantity = forms.IntegerField(label='Liczba biletów', min_value=1, max_value=10)
